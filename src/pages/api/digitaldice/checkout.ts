5cf5dc1f13738832d16ac03b018ceed3b196da2f
oliveira-victor <victor.freire.oliveira@gmail.com>
2024-02-13 15:19:11 -0300

    first commit

---
+++ src/pages/api/digitaldice/checkout.ts
@@ -0,0 +1,103 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import Cors from 'cors'
+
+const cors = Cors({
+  methods: ['POST', 'GET', 'HEAD'],
+})
+
+function runMiddleware(
+  req: NextApiRequest,
+  res: NextApiResponse,
+  fn: Function
+) {
+  return new Promise((resolve, reject) => {
+    fn(req, res, (result: any) => {
+      if (result instanceof Error) {
+        return reject(result)
+      }
+
+      return resolve(result)
+    })
+  })
+}
+
+function validate(body: any, res: any) {
+  if (!body) {
+    return false
+  } 
+
+  let isValid = true;
+  
+  try {
+    if (body.payment.card.active) {
+      isValid = body.payment.card.number.length > 0
+    }
+
+    if (body.products.length === 0) {
+      isValid = false
+    }
+  } catch(e) {
+    return false
+  }
+
+  return isValid
+}
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<any>
+) {
+  const randomId = parseInt((Math.random() * 100).toFixed(), 10);
+  const payload = {
+    products: [
+      {
+        id: 1,
+        price: 0,
+      }
+    ],
+    delivery: {
+      receiver: "string",
+      address: {
+        description: "string",
+        email: "string",
+        city: "string",
+        state: "string",
+        country: "string",
+        zipCode: "string",
+        number: 12,
+      }
+    },
+    payment: {
+      card: {
+        name: "string",
+        number: "string",
+        code: 123,
+        expires: {
+          month: 12,
+          year: 1234,
+        },
+      },
+    },
+  };
+  await runMiddleware(req, res, cors)
+
+  if (req.method === 'GET') {
+    res.status(200).json({
+      ...payload,
+    })
+  } else if (req.method === 'POST') {
+    if (validate(req.body, res)) {
+      res.status(201).json({
+        orderId: `#256${randomId}`
+      })
+    } else {
+      res.status(400).json({
+        message: 'Review the data sent'
+      })
+    }
+  } else {
+    res.status(400).json({
+      message: 'Method not implemented'
+    })
+  }
+}
